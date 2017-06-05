e27d7652940762a934bad1e39a24a61bf0e66b74
Pi-Ay <muscaders@gmail.com>
2017-06-05 20:39:48 +0200

    Fix last commit

---
+++ Assets/Resources/Scripts/ButtonHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour {
+
+    public short type = 0;
+
+	// Use this for initialization
+	void Start () {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(Handler);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Handler() {
+        GameLogicClient.game.buttonInput(type);
+    }
+}
