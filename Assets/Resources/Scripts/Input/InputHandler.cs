41c0a559c5910676f5d1e03b7a4f434364cf5051
Daratrixx <zeratul_du_38@hotmail.fr>
2017-09-14 16:56:23 +0200

    Cleaned Input. More cleaning in Tools.

--- Assets/Resources/Scripts/Input/InputHandler.cs
+++ Assets/Resources/Scripts/Input/InputHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Logic;
 
 public class InputHandler : MonoBehaviour {
--- Assets/Resources/Scripts/Tools/StringParsingTool.cs
+++ Assets/Resources/Scripts/Tools/StringParsingTool.cs
@@ -1,7 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
-
-namespace Tools {
+﻿namespace Tools {
 
     public class StringParsingTool {
 
