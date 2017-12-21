ca98fb6b73b4a73d4b5f769a64a0f4aa8b9a4e80
Daratrixx <zeratul_du_38@hotmail.fr>
2017-12-21 12:27:05 +0100

    WIP - improve game interface view and controll.

---
+++ Assets/CharacterViewController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterViewController : MonoBehaviour {
+
+    public Data.Entity entity;
+    public Image HPFill;
+    public Text HPValue;
+    public Text APValue;
+    public Text MPValue;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void UpdateStats(Data.Entity e) {
+        UpateHP(e.currentHealth, e.maxHealth);
+        UpateAP(e.currentAP, e.maxAP);
+        UpateMP(e.currentMP, e.maxMP);
+    }
+
+    void UpateHP(int current, int max) {
+        HPFill.fillAmount = (float)current / (float)max;
+        HPValue.text = "" + current + "/" + max;
+    }
+
+    void UpateAP(int current, int max) {
+
+        APValue.text = "" + current;
+    }
+
+    void UpateMP(int current, int max) {
+
+        MPValue.text = "" + current;
+    }
+}
---
+++ Assets/GameViewController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameViewController : MonoBehaviour {
+
+    public GameObject rootPanel;
+
+    public GameObject quickActionPanel;
+    public GameObject movementActionPanel;
+    public GameObject slowActionPanel;
+    public Image quickSpell1;
+    public Image quickSpell2;
+    public Image quickSpell3;
+    public Image quickSpell4;
+    public Image slowSpell1;
+    public Image slowSpell2;
+    public Image slowSpell3;
+    public Image slowSpell4;
+
+    private GameObject currentPanel;
+
+    // Use this for initialization
+    void Start() {
+        currentPanel = rootPanel;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void SwitchToRoot() {
+        currentPanel.SetActive(false);
+        currentPanel = rootPanel;
+        currentPanel.SetActive(true);
+    }
+
+    public void SwitchToQuickAction() {
+        currentPanel.SetActive(false);
+        currentPanel = quickActionPanel;
+        currentPanel.SetActive(true);
+    }
+
+    public void SwitchToMovementAction() {
+        currentPanel.SetActive(false);
+        currentPanel = movementActionPanel;
+        currentPanel.SetActive(true);
+    }
+
+    public void SwitchToSlowAction() {
+        currentPanel.SetActive(false);
+        currentPanel = slowActionPanel;
+        currentPanel.SetActive(true);
+    }
+
+    public void SelectSpell1() {
+
+    }
+
+    public void SelectSpell2() {
+
+    }
+
+    public void SelectSpell3() {
+
+    }
+
+    public void SelectSpell4() {
+
+    }
+
+    public void Cancel() {
+
+    }
+
+    public void Confirm() {
+
+    }
+
+}
